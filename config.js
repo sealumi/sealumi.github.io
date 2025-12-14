42b57fb8cf6884619bb5746feebeec090619994f
sirshiru <cantaloupesyummy@gmail.com>
2025-12-14 22:18:30 +0100

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx7367gpJyceY7C6Kg7msUw0t66nL_hbUkM0Nyrl-aBIucO0to3CaI4xZwaQHUeyVVEdw/exec";
-const API_KEY = "7kQm9xL2pNfR8vT";
-const HOSTER_CODE = "kitchen-verification-17";
