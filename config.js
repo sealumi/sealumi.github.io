639d6226dbee1d91f3132c40c09ef04ee8c8d593
sealumi <cantaloupesyummy@gmail.com>
2025-12-14 22:05:37 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyGjePNo5Q8GcSNdrahd2ffpY_37U3C44iqD1nE71VnvSv6lA9myI6ipahkhChoZdzOOw/exec";
+const API_KEY = "7kQm9xL2pNfR8vT";
+const HOSTER_CODE = "kitchen-control-17";
