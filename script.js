c89b0123e7c45bb10a2d17429909ef77ecc5ef33
sealumi <cantaloupesyummy@gmail.com>
2025-12-14 22:05:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx7367gpJyceY7C6Kg7msUw0t66nL_hbUkM0Nyrl-aBIucO0to3CaI4xZwaQHUeyVVEdw/exec";
-const API_KEY = "7kQm9xL2pNfR8vT";
-const HOSTER_CODE = "kitchen-verification-17";
 
 const ALLOWED_RANKS = [
   "Kitchen Trainee",
@@ -121,5 +118,6 @@ function showStatus(type, message) {
   statusEl.textContent = message;
   statusEl.style.display = 'block';
 }
+
 
 
